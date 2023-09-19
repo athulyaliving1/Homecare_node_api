b7f474438378f2310cd5b778936242cf618f223f
athulyaliving1 <muthukumar@athulyaliving.com>
2023-09-19 18:54:25 +0530

    FIX:url fix

--- src/utilities/Url.js
+++ src/utilities/Url.js
@@ -1,4 +1,4 @@
-export const URLDevelopment =
-  ("Access-Control-Allow-Origin", "http://localhost:4042");
 // export const URLDevelopment =
-//   ("Access-Control-Allow-Origin", "http://164.52.220.10:4042");
+//   ("Access-Control-Allow-Origin", "http://localhost:4042");
+export const URLDevelopment =
+  ("Access-Control-Allow-Origin", "http://164.52.220.10:4042");
